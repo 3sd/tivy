a266d80f597e03cad26b47cc01e7392645ed50d8
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-06-06 22:59:11 +0100

    some modern css

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,11 +1,11 @@
-<div class="tivy-flex">
-  <div class="tivy-main">
+<div class="tivy-grid-container">
+  <div class="tivy-grid-menu">
+    <?php print render($page['left']); ?>
+  </div>
+  <div class="tivy-grid-main">
     <h1 class="title" id="page-title"><?php print $title; ?></h1>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php if ($messages): ?><?php print $messages; ?><?php endif; ?>
-    <?php print render($page['main']); ?>
-  </div>
-  <div class="tivy-right">
-    <?php print render($page['right']); ?>
+    <?php print render($page['content']); ?>
   </div>
 </div>
