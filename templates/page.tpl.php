cd29bfe6d426cc617d90e7bcbc8c1770c123a33d
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-06-14 09:24:05 +0100

    adding breadcrumb

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -3,6 +3,7 @@
     <?php print render($page['left']); ?>
   </div>
   <div class="tivy-grid-main">
+    <?php print $breadcrumb; ?>
     <h1 class="title" id="page-title"><?php print $title; ?></h1>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php if ($messages): ?><?php print $messages; ?><?php endif; ?>
